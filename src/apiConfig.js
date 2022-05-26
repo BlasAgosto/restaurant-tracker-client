9f0bac8af699dd497240e5c327e2f0b588a44688
Jill Agosto <blasagosto@live.com>
2022-05-26 15:15:44 -0500

    added deployment link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: ' https://glacial-atoll-96940.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
